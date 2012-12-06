d0584e3c54518a66b0047f1210e3376f4a486f57
Eyal Shahar <eyal@shahar.co.il>
2012-12-07 00:10:26 +0200

    Changed from image elements to divs with backgrounds, to cover the entire page.

--- index.php
+++ index.php
@@ -6,10 +6,17 @@
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
 	</head>
 	<body>
-	<img id="shot" src="cover.png"/><img id="standbyShot"/>
+	<div id="shot"></div>
+	<div id="standbyShot"></div>
 	<style>
-		body{margin:0; background-color:black}
-		#shot, #standbyShot{width:800px; height:600px;position:absolute; left:0; top:0;}
+		body{margin:0; background-color:blue}
+		#shot, #standbyShot{
+			background:url(cover.png) no-repeat center center fixed; 
+			-webkit-background-size: cover;
+			-moz-background-size: cover;
+			-o-background-size: cover;
+			background-size: cover;
+		width:100%; height:100%;position:fixed; left:0; top:0;}
 		#shot{z-index:10;}
 		#astandbyShot{visibility:hidden; width:0; height:0;}
 	</style>
@@ -34,7 +41,8 @@ function changeImage(){
 	//setTimer(function(){},500);
 	
 	$('#shot').fadeOut(1000,function(){
-			$(this).attr("src", $('#standbyShot').attr("src"));
+			$(this).css("background-image", $('#standbyShot').css("background-image"));
+			console.log($('#standbyShot').css("background-image"));
 			$(this).show();
 			setNextShot();
 	});
@@ -42,9 +50,14 @@ function changeImage(){
 }
 
 function setNextShot(){
-	$('#standbyShot').attr("src", shots[Math.floor(Math.random() * totalShots)]);
+	var nextShotUrl = shots[Math.floor(Math.random() * totalShots)];
+	$('#standbyShot').css("background-image", 'url('+nextShotUrl+')');
 }
 setNextShot();
+
+function setShotsSize(){
+	$('#shot').css({width:window.width,height:window.height});
+}
 </script>
 
 </body>
